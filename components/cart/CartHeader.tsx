5692668aae340401042997187610c639ff51614d
KIM-DONGJU <kdj0689@gmail.com>
2024-06-24 15:32:14 +0900

    cart

---
+++ components/cart/CartHeader.tsx
@@ -0,0 +1,5 @@
+function CartHeader() {
+  return <h1>장바구니 페이지</h1>;
+}
+
+export default CartHeader;
--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,5 +1,27 @@
-function CardPage() {
-  return <div>장바구니 페이지</div>;
+import { fetchCarts } from '@/api';
+import CartHeader from '@/components/cart/CartHeader';
+import CartList from '@/components/cart/CartList';
+import { InferGetServerSidePropsType } from 'next';
+
+function CardPage({
+  carts,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <div>
+      <CartHeader />
+      <CartList carts={carts} />
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const { data } = await fetchCarts();
+
+  return {
+    props: {
+      carts: data,
+    },
+  };
 }
 
 export default CardPage;
